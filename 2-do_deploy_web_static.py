98d282d31c32cf1761677a8be8ade35deb5d77d7
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-07-06 09:04:41 +0200

    Fix: Improving the second exercise

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,43 @@
 #!/usr/bin/python3
-from fabric.api import *
-import os
-from datetime import datetime
+from fabric.api import env, run, put
+from os.path import exists
 
-env.hosts = ['35.231.59.48', '35.237.144.156']
-
+env.hosts = ['<IP web-01>', '<IP web-02>']
+env.user = '<your-username>'
+env.key_filename = '<path-to-your-ssh-key>'
 
 def do_deploy(archive_path):
-    a_list = archive_path.split(".tgz")
-    archive_wo_ext = "".join(a_list)
-    b_list = archive_wo_ext.split("versions/")
-    archive_wo_ext_ver = "".join(b_list)
-    c_list = archive_path.split("versions/")
-    archive_wo_ver = "".join(c_list)
-    if archive_path:
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".
-            format(archive_wo_ext_ver))
-        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_wo_ver, archive_wo_ext_ver))
-        run("rm -r /tmp/{}".format(archive_wo_ver))
-        run("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(archive_wo_ext_ver,
-                                              archive_wo_ext_ver))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-            format(archive_wo_ext_ver))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(archive_wo_ext_ver))
-        print("New version deployed!")
-        return True
-    else:
+    """
+    Distributes an archive to web servers.
+    """
+    if not exists(archive_path):
         return False
 
-
-def do_pack():
     try:
-        filepath = "versions/web_static_" + datetime.now().\
-                   strftime("%Y%m%d%H%M%S") + ".tgz"
-        local("mkdir -p versions")
-        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static")
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
-    except:
-        return None
+        archive_filename = archive_path.split('/')[-1]
+        archive_folder = "/data/web_static/releases/{}".format(archive_filename.split('.')[0])
+
+        # Upload the archive to /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress the archive to /data/web_static/releases/
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Move contents to proper location
+        run('mv {}/web_static/* {}/'.format(archive_folder, archive_folder))
+        run('rm -rf {}/web_static'.format(archive_folder))
+
+        # Delete the symbolic link /data/web_static/current
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link /data/web_static/current
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
